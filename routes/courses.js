d8abf2e009a49ce07bbb858d478dc07c8052f66c
Nina Tymińska <nina.tyminska@k2.pl>
2019-06-27 08:23:54 +0200

    WIP Code refactor and validation for forms.

--- routes/courses.js
+++ routes/courses.js
@@ -1,37 +1,50 @@
-var express      = require("express"),
+var express      = require('express'),
     router       = express.Router({mergeParams: true}),
-    Course       = require("../models/course"),
-    Comment      = require("../models/comment"),
-    User         = require("../models/user"),
-    Notification = require("../models/notification"),
-    middleware   = require("../middleware"),
-    Review       = require("../models/review");
+    {check, validationResult} = require('express-validator/check'),
+    Course       = require('../models/course'),
+    Comment      = require('../models/comment'),
+    User         = require('../models/user'),
+    Notification = require('../models/notification'),
+    middleware   = require('../middleware'),
+    Review       = require('../models/review');
     
-router.get("/", function(req, res) {
-    Course.find({}).sort({date: 1}).exec(function(err, allCourses) {
-        if(err) {
-            req.flash("error", "Wystąpił błąd.");
-            console.log(err);
-        } else {
-            res.render("courses/courses", {courses : allCourses});
-        }
-    });
-});
+router.get('/', async (req, res) => {
+    try {
+        let allCourses = await Course.find({}).sort({date: 1}).exec();
+        res.render('courses/courses', {courses : allCourses});
+    } catch(err) {
+        req.flash('error', 'Wystąpił błąd.');
+        return res.redirect('back');
+    }
+});    
 
-router.post("/", middleware.isLoggedIn, async function(req, res){
-    var name = req.body.name;
-    var image = req.body.image;
-    var desc = req.sanitize(req.body.description);
-    var web = req.body.website;
-    var cat = req.body.category;
-    var date = req.body.date;
-    var author = {
+router.post('/', middleware.isLoggedIn, [
+    check('name', 'Tytuł jest za krótki.').isLength({ min: 3 }),
+    check('image', 'Podaj URL obrazka.').isURL(),
+    check('desc', 'Opis jest za krótki.').isLength({ min: 20 }),
+    check('web', 'Podaj prawidłowy adres URL.').isURL()
+], async (req, res) => {
+    var name   = req.body.name,
+        image  = req.body.image,
+        desc   = req.sanitize(req.body.description),
+        web    = req.body.website,
+        cat    = req.body.category,
+        date   = req.body.date,
+        author = {
         id: req.user._id,
         username: req.user.username,
         avatar: req.user.avatar
     };
+    var formErrors = validationResult(req);
+    if (!formErrors.isEmpty()) {
+                let arrayFormErrors = await formErrors.mapped();
+                let errorsMsg = await arrayFormErrors;
+                console.log(arrayFormErrors);
+                
+                res.redirect({errors: errorsMsg}, 'back');
+                req.flash('error', 'Wystąpił błąd.');
+        } else {
     var newCourse = {name: name, image: image, description: desc, website: web, author: author, category: cat, date: date};
-
     try {
       let course = await Course.create(newCourse);
       let user = await User.findById(req.user._id).populate('followers').exec();
@@ -47,83 +60,87 @@ router.post("/", middleware.isLoggedIn, async function(req, res){
         follower.notifications.push(notification);
         follower.save();
       }
-      req.flash("success", "Kurs dodany.");
+      req.flash('success', 'Kurs dodany.');
       res.redirect(`/c/${course.id}`);
-    } catch(err) {
-      req.flash("error", "Wystąpił błąd.");
-      res.redirect('back');
+    } catch(formErrors) {
+        
     }
+  }  
 });
 
-router.get("/new", middleware.isLoggedIn, function(req, res) {
-    res.render("courses/new");
+router.get('/new', middleware.isLoggedIn, (req, res) => {
+    res.render('courses/new');
 });
 
-router.get("/c/:id", function(req, res) {
-   Course.findById(req.params.id).populate("comments").populate({
-        path: "reviews",
+router.get('/c/:id', (req, res) => {
+   Course.findById(req.params.id).populate('comments').populate({
+        path: 'reviews',
         options: {sort: {createdAt: -1}}
-    }).exec(function (err, foundCourse) {
+    }).exec((err, foundCourse) => {
         if (err) {
-            req.flash("error", "Wystąpił błąd.");
+            req.flash('error', 'Wystąpił błąd.');
             console.log(err);
         } else {
-            Course.find({}, function(err, allCourses) {
+            Course.find({}, (err, allCourses) => {
                 if(err) {
-                    req.flash("error", "Wystąpił błąd.");
+                    req.flash('error', 'Wystąpił błąd.');
                     console.log(err);
                 } else {
-                     res.render("courses/show", {course: foundCourse, courses: allCourses});
+                     res.render('courses/show', {course: foundCourse, courses: allCourses});
                 }
             });
         }
     });
 });
 
-router.get("/c/:id/edit", middleware.checkCourseOwner, function(req, res) {
-   Course.findById(req.params.id, function(err, foundCourse) {
-       if(err || !foundCourse) {
-           req.flash("error", "Kurs nie został znaleziony.");
-           res.redirect("back");
-        } else {
-            res.render("courses/edit", {course : foundCourse});
+router.get('/c/:id/edit', middleware.checkCourseOwner, async (req, res) => {
+    try {    
+        let foundCourse = await Course.findById(req.params.id).exec();
+        res.render('courses/edit', {course : foundCourse});
+        if (!foundCourse) {
+            throw req.flash('error', 'Kurs nie został znaleziony.');
+            res.redirect('back');
         }
-    });
+    } catch(err){
+            req.flash('error', 'Wystąpił błąd.');
+            res.redirect('back');
+    }
+});    
+
+router.put('/c/:id', middleware.checkCourseOwner, async (req, res) => {
+    try {
+        let updatedCourse = Course.findByIdAndUpdate(req.params.id, req.body.course).exec();
+        req.flash('success', 'Kurs zaktualizowany.');
+        res.redirect('/c/' + req.params.id);
+    } catch(err) {
+        req.flash('error', 'Wystąpił błąd.');
+        res.redirect('back');
+    }
 });
 
-router.put("/c/:id", middleware.checkCourseOwner, function(req, res) {
-    Course.findByIdAndUpdate(req.params.id, req.body.course, function(err, updatedCourse) {
-        if(err) {
-            res.redirect("back");
-        } else {
-            req.flash("success", "Kurs zaktualizowany.");
-            res.redirect("/c/" + req.params.id);
-        }
-    });
-});
-
-router.delete("/c/:id", middleware.checkCourseOwner, function (req, res) {
-    Course.findById(req.params.id, function (err, course) {
+router.delete('/c/:id', middleware.checkCourseOwner, (req, res) => {
+    Course.findById(req.params.id, (err, course) => {
         if (err) {
-            res.redirect("/");
+            req.flash('error', 'Wystąpił błąd.');
+            res.redirect('/');
         } else {
-            Comment.deleteOne({"_id": {$in: course.comments}}, function (err) {
+            Comment.deleteOne({'_id': {$in: course.comments}}, err => {
                 if (err) {
-                    req.flash("error", "Wystąpił błąd.");
-                    return res.redirect("/");
+                    req.flash('error', 'Wystąpił błąd.');
+                    return res.redirect('/');
                 }
-                Review.deleteOne({"_id": {$in: course.reviews}}, function (err) {
+                Review.deleteOne({'_id': {$in: course.reviews}}, err => {
                     if (err) {
-                        req.flash("error", "Wystąpił błąd.");
-                        return res.redirect("/");
+                        req.flash('error', 'Wystąpił błąd.');
+                        return res.redirect('/');
                     }
                     course.remove();
-                    req.flash("success", "Kurs został usunięty.");
-                    res.redirect("/");
+                    req.flash('success', 'Kurs został usunięty.');
+                    res.redirect('/');
                 });
             });
         }
     });
-});
+});     
 
 module.exports = router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,30 +1,30 @@
-var express    = require("express"),
+var express    = require('express'),
     router     = express.Router({mergeParams: true}),
-    Course     = require("../models/course"),
-    Review     = require("../models/review"),
-    middleware = require("../middleware");
+    Course     = require('../models/course'),
+    Review     = require('../models/review'),
+    middleware = require('../middleware');
     
 function calculateAverage(reviews) {
     if (reviews.length === 0) {
         return 0;
     }
     var sum = 0;
-    reviews.forEach(function (element) {
+    reviews.forEach(element => {
         sum += element.rating;
     });
     return sum / reviews.length;
 }
 
-router.post("/", middleware.isLoggedIn, middleware.checkReviewExistence, function (req, res) {
-    Course.findById(req.params.id).populate("reviews").exec(function (err, course) {
+router.post('/', middleware.isLoggedIn, middleware.checkReviewExistence, (req, res) => {
+    Course.findById(req.params.id).populate('reviews').exec((err, course) => {
         if (err) {
-            req.flash("error", "Wystąpił błąd.");
-            return res.redirect("back");
+            req.flash('error', 'Wystąpił błąd.');
+            return res.redirect('back');
         }
-        Review.create(req.body.review, function (err, review) {
+        Review.create(req.body.review, (err, review) => {
             if (err) {
-                req.flash("error", "Wystąpił błąd.");
-                return res.redirect("back");
+                req.flash('error', 'Wystąpił błąd.');
+                return res.redirect('back');
             }
             review.author.id = req.user._id;
             review.author.avatar = req.user.avatar;
@@ -34,46 +34,46 @@ router.post("/", middleware.isLoggedIn, middleware.checkReviewExistence, functio
             course.reviews.push(review);
             course.rating = calculateAverage(course.reviews);
             course.save();
-            req.flash("success", "Opinia została dodana.");
+            req.flash('success', 'Opinia została dodana.');
             res.redirect('/c/' + course._id);
         });
     });
 });
 
-router.put("/:review_id", middleware.checkReviewOwnership, function (req, res) {
-    Review.findByIdAndUpdate(req.params.review_id, req.body.review, {new: true}, function (err, updatedReview) {
+router.put('/:review_id', middleware.checkReviewOwnership, (req, res) => {
+    Review.findByIdAndUpdate(req.params.review_id, req.body.review, {new: true}, (err, updatedReview) => {
         if (err) {
-            req.flash("error", "Wystąpił błąd.");
-            return res.redirect("back");
+            req.flash('error', 'Wystąpił błąd.');
+            return res.redirect('back');
         }
-        Course.findById(req.params.id).populate("reviews").exec(function (err, course) {
+        Course.findById(req.params.id).populate('reviews').exec((err, course) => {
             if (err) {
-                req.flash("error", "Wystąpił błąd.");
-                return res.redirect("back");
+                req.flash('error', 'Wystąpił błąd.');
+                return res.redirect('back');
             }
             course.rating = calculateAverage(course.reviews);
             course.save();
-            req.flash("success", "Opinia zaktualizowana.");
+            req.flash('success', 'Opinia zaktualizowana.');
             res.redirect('/c/' + course._id);
         });
     });
 });
 
-router.delete("/:review_id", middleware.checkReviewOwnership, function (req, res) {
-    Review.findByIdAndRemove(req.params.review_id, function (err) {
+router.delete('/:review_id', middleware.checkReviewOwnership, (req, res) => {
+    Review.findByIdAndRemove(req.params.review_id, err => {
         if (err) {
-            req.flash("error", err.message);
-            return res.redirect("back");
+            req.flash('error', err.message);
+            return res.redirect('back');
         }
-        Course.findByIdAndUpdate(req.params.id, {$pull: {reviews: req.params.review_id}}, {new: true}).populate("reviews").exec(function (err, course) {
+        Course.findByIdAndUpdate(req.params.id, {$pull: {reviews: req.params.review_id}}, {new: true}).populate('reviews').exec((err, course) => {
             if (err) {
-                req.flash("error", err.message);
-                return res.redirect("back");
+                req.flash('error', err.message);
+                return res.redirect('back');
             }
             course.rating = calculateAverage(course.reviews);
             course.save();
-            req.flash("success", "Opinia usunięta.");
-            res.redirect("/c/" + req.params.id);
+            req.flash('success', 'Opinia usunięta.');
+            res.redirect('/c/' + req.params.id);
         });
     });
 });
