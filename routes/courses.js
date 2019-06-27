f034b9ad66937f8b5222f2bb61f4d91bb24e57ad
Nina Tymińska <nina.tyminska@k2.pl>
2019-06-28 00:11:08 +0200

    Validation update.

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,6 @@
 var express      = require('express'),
     router       = express.Router({mergeParams: true}),
-    {check, validationResult} = require('express-validator/check'),
+    {check, validationResult} = require('express-validator'),
     Course       = require('../models/course'),
     Comment      = require('../models/comment'),
     User         = require('../models/user'),
@@ -21,51 +21,49 @@ router.get('/', async (req, res) => {
 router.post('/', middleware.isLoggedIn, [
     check('name', 'Tytuł jest za krótki.').isLength({ min: 3 }),
     check('image', 'Podaj URL obrazka.').isURL(),
-    check('desc', 'Opis jest za krótki.').isLength({ min: 20 }),
-    check('web', 'Podaj prawidłowy adres URL.').isURL()
+    check('description', 'Opis jest za krótki.').isLength({ min: 20 }),
+    check('website', 'Podaj prawidłowy adres URL.').isURL()
 ], async (req, res) => {
-    var name   = req.body.name,
-        image  = req.body.image,
-        desc   = req.sanitize(req.body.description),
-        web    = req.body.website,
-        cat    = req.body.category,
-        date   = req.body.date,
-        author = {
-        id: req.user._id,
-        username: req.user.username,
-        avatar: req.user.avatar
-    };
-    var formErrors = validationResult(req);
-    if (!formErrors.isEmpty()) {
-                let arrayFormErrors = await formErrors.mapped();
-                let errorsMsg = await arrayFormErrors;
-                console.log(arrayFormErrors);
-                
-                res.redirect({errors: errorsMsg}, 'back');
-                req.flash('error', 'Wystąpił błąd.');
+        var name   = req.body.name,
+            image  = req.body.image,
+            desc   = req.sanitize(req.body.description),
+            web    = req.body.website,
+            cat    = req.body.category,
+            date   = req.body.date,
+            author = {
+            id: req.user._id,
+            username: req.user.username,
+            avatar: req.user.avatar
+        };
+        var newCourse = {name: name, image: image, description: desc, website: web, author: author, category: cat, date: date};
+        var formErrors = validationResult(req);
+        if (!formErrors.isEmpty()) {
+                    let arrayFormErrors = await formErrors.mapped();
+                    let errorsMsg = await arrayFormErrors;
+                    console.log(arrayFormErrors);
+                    res.render('courses/new-val.ejs', {errors: errorsMsg, newCourse: newCourse});             
         } else {
-    var newCourse = {name: name, image: image, description: desc, website: web, author: author, category: cat, date: date};
-    try {
-      let course = await Course.create(newCourse);
-      let user = await User.findById(req.user._id).populate('followers').exec();
-      let newNotification = {
-        username: req.user.username,
-        userId: req.user._id,
-        avatar: req.user.avatar,
-        courseId: course.id,
-        courseName: course.name
-      };
-      for(const follower of user.followers) {
-        let notification = await Notification.create(newNotification);
-        follower.notifications.push(notification);
-        follower.save();
-      }
-      req.flash('success', 'Kurs dodany.');
-      res.redirect(`/c/${course.id}`);
-    } catch(formErrors) {
-        
-    }
-  }  
+            try {
+                let course = await Course.create(newCourse);
+                let user = await User.findById(req.user._id).populate('followers').exec();
+                let newNotification = {
+                    username: req.user.username,
+                    userId: req.user._id,
+                    avatar: req.user.avatar,
+                    courseId: course.id,
+                    courseName: course.name
+                };
+                for(const follower of user.followers) {
+                    let notification = await Notification.create(newNotification);
+                    follower.notifications.push(notification);
+                    follower.save();
+                }
+                req.flash('success', 'Kurs dodany.');
+                res.redirect(`/c/${course.id}`);
+            } catch(formErrors) {
+                req.flash('error', 'Wystąpił błąd.');
+                }
+        }  
 });
 
 router.get('/new', middleware.isLoggedIn, (req, res) => {
